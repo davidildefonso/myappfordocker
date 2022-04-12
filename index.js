3964b223e77a5e05ca769e80fc96d0dab6894b37
David Ildefonso <david_lion_ig@outlook.com.pe>
2022-04-12 14:03:54 -0500

    corrected script

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 const scriptPath = './script.sh'
-exec(`sh ${scriptPath}`, (err, stdout, stderr) => {
-	if(err){
-		console.error(err)
-		return
-	}
-	console.log(stdout, stderr)
-})
+spawn('sh', [scriptPath], { stdio: 'inherit' })
--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 echo "Enter something"
+read 0 1
 echo "Hello bro! you entered ${1}" 
