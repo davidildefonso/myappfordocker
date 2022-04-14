4ac4c08d73ab39db738c55e2e49d9253fcdfd316
David Ildefonso <david_lion_ig@outlook.com.pe>
2022-04-14 17:54:33 -0500

    added mongodb connection

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const port = 3000
 const app = express()
 const path = require('path')
+const { connection, closeConnection } = require(path.join(__dirname, 'src/db/connection.js'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(port, () => {
+app.listen(port, async () => {
 	console.log(`app listening on port ${port}`)
+	try {
+		await connection()
+		console.log('connected')
+	} catch (error) {
+		console.error('error:', error)
+	}finally{
+		closeConnection()
+		console.log('connection closed')
+	}
 })
